190d14e0bd815ab9d9aeca3021f0c072bcfa3f85
Ed Mckain <mckain.default@gmail.com>
2018-02-23 23:40:10 +0100

    adds basic structure based on Spring Initializr

---
+++ src/main/java/com/hacknplay/transactionstats/TransactionStatsApplication.java
@@ -0,0 +1,12 @@
+package com.hacknplay.transactionstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStatsApplication.class, args);
+	}
+}
