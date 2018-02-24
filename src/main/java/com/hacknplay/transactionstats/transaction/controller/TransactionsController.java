967b7593e4b2258b946e6c069bf8e52097476d6d
Ed Mckain <mckain.default@gmail.com>
2018-02-24 23:06:28 +0100

    uses @RequestMapping for new transactions

--- src/main/java/com/hacknplay/transactionstats/transaction/controller/TransactionsController.java
+++ src/main/java/com/hacknplay/transactionstats/transaction/controller/TransactionsController.java
@@ -1,4 +1,19 @@
 package com.hacknplay.transactionstats.transaction.controller;
 
+import com.hacknplay.transactionstats.transaction.model.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 public class TransactionsController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/transactions")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void create(@RequestParam("amount") Double amount, @RequestParam("timestamp") Long timestamp){
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        transaction.setTimestamp(timestamp);
+    }
+
 }
--- src/main/java/com/hacknplay/transactionstats/transaction/model/Transaction.java
+++ src/main/java/com/hacknplay/transactionstats/transaction/model/Transaction.java
@@ -3,14 +3,31 @@ package com.hacknplay.transactionstats.transaction.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Transaction {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Double amount;
     private Long timestamp;
+
+    public Double getAmount(){
+        return amount;
+    }
+
+    public void setAmount(Double amount){
+        this.amount = amount;
+    }
+
+    public Long getTimestamp(){
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp){
+        this.timestamp = timestamp;
+    }
 }
